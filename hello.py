dac8a3f8f5ed2a9dab109107fa8345822964ec5f
lroslonek <lukasz.roslonek@gmail.com>
2024-08-09 20:12:49 +0200

    wtf forms + messages

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc"
+
+
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
@@ -13,8 +22,16 @@ def user(name):
     return render_template("user.html", name=name)
 
 
-@app.errorhandler(404)
-def page_not_fount(e):
-    return render_template("404.html"), 404
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('Form submitted successfully')
+    return render_template("name.html", name=name, form=form)
 
-
+    @app.errorhandler(404)
+    def page_not_fount(e):
+        return render_template("404.html"), 404
